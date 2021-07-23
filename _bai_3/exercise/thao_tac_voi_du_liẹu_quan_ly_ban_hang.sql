c7ec5b0815313a86c7ddc7ca30e73287767e5d97
nhatlinh8816 <dangnhatlinh0213@gmail.com>
2021-07-23 08:10:07 +0700

    bai_tap_23_07

--- _bai_3/exercise/thao_tac_voi_du_liẹu_quan_ly_ban_hang.sql
+++ _bai_3/exercise/thao_tac_voi_du_liẹu_quan_ly_ban_hang.sql
@@ -7,13 +7,18 @@ insert into product
 value (1,'May Giat',3),(2,'Tu Lanh',5),(3,'Dieu Hoa',7),(4,'Quat',1),(5,'Bep Dien',2);
 insert into order_detail 
 value (1,1,3),(1,3,7),(1,4,2),(2,1,1),(3,1,8),(2,5,4),(2,3,3);
-select order_id,customer_customer_id,order_date from `order`;
+select order_id,customer_customer_id,order_date 
+from `order`;
 select customer.customer_id,customer.customer_name,product.product_id,product.product_name
-from customer join `order` on customer.customer_id = `order`.customer_customer_id join order_detail on  `order`.order_id = orderdetail.order_order_id
+from customer join `order` on customer.customer_id = `order`.customer_customer_id 
+join order_detail on  `order`.order_id = orderdetail.order_order_id
 join product on order_detail.product_product_id = product.product_id;
-select * from customer where customer_id not in (select customer_customer_id from `order`);
+select * 
+from customer 
+where customer_id not in (select customer_customer_id from `order`);
 select order_detail.order_order_id,`order`.order_date,sum(order_detail_qty*product.product_price) as order_total_price  
-from `order` join order_detail  on `order`.order_id = order_detail.order_order_id
+from `order` 
+join order_detail  on `order`.order_id = order_detail.order_order_id
 join product on order_detail.product_product_id = product.product_id
 group by order_detail.order_order_id;
 
